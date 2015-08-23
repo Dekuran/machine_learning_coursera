0fc61b7c46b3d91d5a10916433f2b0dd2e2f79a1
Dekuran <declan.clowry@gmail.com>
2015-08-23 14:14:54 +0100

    fixed model and knitted html need to go back and sort out set seed

--- exploratory analysis.R
+++ exploratory analysis.R
@@ -18,37 +18,56 @@ summary(train)
 #removing the rownumbers, timestamps, window, and usernames as variables
 exercise<-subset(train,select=-c(X,user_name,raw_timestamp_part_1,raw_timestamp_part_2,cvtd_timestamp,new_window,num_window))
 # removing mostly NA data
+dtraining<-exercise
+nzv<-nearZeroVar(dtraining, saveMetrics=TRUE)
+nzv<-subset(nzv, nzv==TRUE)     # a subset of 60 columns of the original data
+dtraining<-subset(dtraining, select = !names(dtraining) %in% rownames(nzv))
+dNA<-function(vector){ return(sum(is.na(vector))) }
+NAnum<-sapply(dtraining, dNA)
+noNA<-names(NAnum[NAnum / nrow(dtraining) == 0])
+dtraining<-subset(dtraining, select = names(dtraining) %in% noNA)
+
+
 few_na<-apply(!is.na(exercise),2,sum)>19621
 exercise<-exercise[,few_na]
 #splitting the training data into training and testing data to allow some pre-testing before using the real test set
-inTrain<-createDataPartition(y=exercise$classe,p=0.7,list=F)
-training<-exercise[inTrain,]
-testing<-exercise[-inTrain,]
-inTrainsmall<-createDataPartition(y=exercise$classe,p=0.3,list=F)
+preProVals <-preProcess(dtraining[,-c(1,55)], method = c("center", "scale"))
+preProVals$user_name <- dtraining$user_name
+preProVals$classe <- dtraining$classe
+toTrain <- createDataPartition(preProVals$classe, p=0.75, list=F) #p = default value.check it!
+training<-dtraining[toTrain,]
+testing<-dtraining[-toTrain]
+
+inTrain<-createDataPartition(y=dtraining$classe,p=0.7,list=F)
+training<-dtraining[inTrain,]
+testing<-dtraining[-inTrain,]
+inTrainsmall<-createDataPartition(y=exercise$classe,p=0.1,list=F)
 trainingsmall<-exercise[inTrainsmall,]
+testingbig<-exercise[-inTrainsmall,]
+
+summary(training)
+qplot(data=training, accel_dumbbell_z, accel_forearm_z, colour=classe)
 ## apply random forest method
-modFit<-train(classe ~. , 
-              data= training, 
-              method="rf" ,
-              trControl=trainControl(method="cv",number=5),
-              prox=TRUE,
-              allowParallel=TRUE)
+modFit<-train(classe ~. , data= trainingsmall, method="rf" , trControl=trainControl(method="cv",number=5),prox=TRUE, allowParallel=TRUE)
+modFit2<-train(classe ~. , data= training , method="knn", trControl=trainControl(method="cv",number=5),  allowParallel=TRUE)
+## so slow! try using knn maybe? adaboost?
+
 print(modFit)
 print(modFit$finalModel)
-pred<-predict(modFit$finalModel, newdata=testing)
-testing<-complete.cases(testing)
+pred<-predict(modFit$finalModel, newdata=testingbig)
+testing<-complete.cases(testingbig)
 testing$predRight<- pred == testing$classe
 table(pred,testing$classe)
 pred
 testing$classe
 
 ## now let's get the real test data
-trainurl<-'https://d396qusza40orc.cloudfront.net/predmachlearn/pml-testing.csv'
+testurl<-'https://d396qusza40orc.cloudfront.net/predmachlearn/pml-testing.csv'
 download.file(trainurl,destfile="test.csv", method="curl")
 test<-read.csv("test.csv" )
 summary(test)
 #removing the rownumbers, timestamps, window, and usernames as variables
 testing2<-subset(test,select=-c(X,user_name,raw_timestamp_part_1,raw_timestamp_part_2,cvtd_timestamp,new_window,num_window))
 # removing mostly NA data
-few_na<-apply(!is.na(exercise),2,sum)>19621
-exercise<-exercise[,few_na]
+few_na<-apply(!is.na(testing2),2,sum)>19621
+testing2<-testing2[,few_na]
